3c6b44f76efc3066408c2b23eaa1611070e61f99
huy51535 <77768240+huy51535@users.noreply.github.com>
2025-01-17 16:59:33 +0700

    onil-chan no baka
    packet thời tiết ui

---
+++ Assets/Racing UI-pack/Scripts/LoadController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadController : MonoBehaviour {
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3f);
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync("DemoGlobalMap");
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            if (ao.progress == 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
